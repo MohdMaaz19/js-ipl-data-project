44b810d1b7e9def1891a8bfb3d87007d28fc274e
mohdmaaz <mohdmaazshk@gmail.com>
2024-09-18 18:13:51 +0530

    Output formatted

--- src/public/output/6-maximum-player-of-the-match-per-year.js
+++ src/public/output/6-maximum-player-of-the-match-per-year.js
@@ -1,1 +1,42 @@
-{"2008":{"player":"SE Marsh","awardsWon":5},"2009":{"player":"YK Pathan","awardsWon":3},"2010":{"player":"SR Tendulkar","awardsWon":4},"2011":{"player":"CH Gayle","awardsWon":6},"2012":{"player":"CH Gayle","awardsWon":5},"2013":{"player":"MEK Hussey","awardsWon":5},"2014":{"player":"GJ Maxwell","awardsWon":4},"2015":{"player":"DA Warner","awardsWon":4},"2016":{"player":"V Kohli","awardsWon":5},"2017":{"player":"BA Stokes","awardsWon":3}}
+{
+  "2008": {
+    "player": "SE Marsh",
+    "awardsWon": 5
+  },
+  "2009": {
+    "player": "YK Pathan",
+    "awardsWon": 3
+  },
+  "2010": {
+    "player": "SR Tendulkar",
+    "awardsWon": 4
+  },
+  "2011": {
+    "player": "CH Gayle",
+    "awardsWon": 6
+  },
+  "2012": {
+    "player": "CH Gayle",
+    "awardsWon": 5
+  },
+  "2013": {
+    "player": "MEK Hussey",
+    "awardsWon": 5
+  },
+  "2014": {
+    "player": "GJ Maxwell",
+    "awardsWon": 4
+  },
+  "2015": {
+    "player": "DA Warner",
+    "awardsWon": 4
+  },
+  "2016": {
+    "player": "V Kohli",
+    "awardsWon": 5
+  },
+  "2017": {
+    "player": "BA Stokes",
+    "awardsWon": 3
+  }
+}

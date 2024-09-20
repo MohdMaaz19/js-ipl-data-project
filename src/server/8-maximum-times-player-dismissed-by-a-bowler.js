c3609c5d175949b1b591fcdb83a8a1805b2af3d9
mohdmaaz <mohdmaazshk@gmail.com>
2024-09-20 09:39:46 +0530

    Files modified

--- src/server/8-maximum-times-player-dismissed-by-a-bowler.js
+++ src/server/8-maximum-times-player-dismissed-by-a-bowler.js
@@ -2,7 +2,7 @@ import deliveriesData from "../data/deliveriesData.json" assert{type:"json"};
 import * as fs from "fs";
 
 function highestDismissalCount(deliveriesData) {
-  // Step 1: Aggregate dismissals data
+
   const dismissalCounts = deliveriesData.reduce((result, { batsman, bowler, dismissal_kind }) => {
     if (dismissal_kind) {
       if (!result[bowler]) {
@@ -19,7 +19,6 @@ function highestDismissalCount(deliveriesData) {
     return result;
   }, {});
 
-  // Step 2: Find the maximum dismissal count
 
   let maxDismissals = 0;
   let mostFrequentDismissal = { bowler: null, batsman: null };
@@ -33,11 +32,10 @@ function highestDismissalCount(deliveriesData) {
     });
   });
 
-  // Return the result
   return { mostFrequentDismissal, maxDismissals };
 }
 
-// Sample usage with `deliveriesData`
+
 const dataFromFunction = highestDismissalCount(deliveriesData);
 const dataToString = JSON.stringify(dataFromFunction);
 fs.writeFileSync("../public/output/8-maximum-times-player-dismissed-by-a-bowler.json",dataToString);
